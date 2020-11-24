2e9e3a9adfe7a06ec4cd45bc59cf1783c4089c87
ted <tngeene27@gmail.com>
2020-11-24 11:08:07 +0300

    fixed components names

--- layouts/profile.vue
+++ layouts/profile.vue
@@ -7,7 +7,7 @@
   </div>
 </template>
 <script>
-import NavBar from '../components/Navbar'
+import NavBar from '~/components/NavBar'
 import Footer from '~/components/Footer'
 import SideNav from '~/components/SideNav'
 export default {
